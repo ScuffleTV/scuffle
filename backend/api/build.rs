c91422f5a0f2ac9f45b2d65dad700ff350c68cab
Lennart Kloock <lennart.kloock@protonmail.com>
2023-05-30 21:00:13 +0200

    fix: proto compiler call in api build script
    Add allow proto3 flag

--- backend/api/build.rs
+++ backend/api/build.rs
@@ -1,8 +1,14 @@
 const PROTO_DIR: &str = "../../proto";
 
 fn main() {
+    let mut config = prost_build::Config::new();
+
+    config.protoc_arg("--experimental_allow_proto3_optional");
+    config.bytes(["."]);
+
     tonic_build::configure()
-        .compile(
+        .compile_with_config(
+            config,
             &[
                 format!("{}/scuffle/events/ingest.proto", PROTO_DIR),
                 format!("{}/scuffle/backend/api.proto", PROTO_DIR),

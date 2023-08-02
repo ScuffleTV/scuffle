fd97cac44c266438e1982f46001f22af9b0d01ef
Lennart Kloock <lennart.kloock@protonmail.com>
2023-08-02 20:06:35 +0200

    fix(website): simplify tag
    Simplify tag component

--- frontend/website/src/components/tag.svelte
+++ frontend/website/src/components/tag.svelte
@@ -2,106 +2,29 @@
 	export let content: string;
 </script>
 
-<div class="tag">
-	<span>{content}</span>
-	<div class="tag-click" />
-</div>
+<span class="tag">{content}</span>
 
 <style lang="scss">
 	@import "../assets/styles/variables.scss";
 
-	$borderColor: #383838;
-	$dashLength: 0.3rem;
-	$spaceLength: 0.15rem;
-
-	// https://codepen.io/amit_sheen/pen/xxZeyjO for generating the background-image
-
 	.tag {
-		padding: 0.25rem 0.5rem;
+		padding: 0.2rem 0.5rem;
 		font-size: 0.85rem;
-		height: 1.5rem;
 		font-weight: 500;
-		background-image: repeating-linear-gradient(
-				0deg,
-				$borderColor,
-				$borderColor $dashLength,
-				transparent $dashLength,
-				transparent $dashLength + $spaceLength,
-				$borderColor $dashLength + $spaceLength
-			),
-			repeating-linear-gradient(
-				90deg,
-				$borderColor,
-				$borderColor $dashLength,
-				transparent $dashLength,
-				transparent $dashLength + $spaceLength,
-				$borderColor $dashLength + $spaceLength
-			),
-			repeating-linear-gradient(
-				180deg,
-				$borderColor,
-				$borderColor $dashLength,
-				transparent $dashLength,
-				transparent $dashLength + $spaceLength,
-				$borderColor $dashLength + $spaceLength
-			),
-			repeating-linear-gradient(
-				270deg,
-				$borderColor,
-				$borderColor $dashLength,
-				transparent $dashLength,
-				transparent $dashLength + $spaceLength,
-				$borderColor $dashLength + $spaceLength
-			);
-		background-size:
-			1px 100%,
-			100% 1px,
-			1px 100%,
-			100% 1px;
-		background-position:
-			0 0,
-			0 0,
-			100% 0,
-			0 100%;
-		background-repeat: no-repeat;
+		border: 1px dashed #383838;
 		color: $textColorLight;
-		transition: color 0.1s ease-in-out;
-
-		cursor: default;
+		transition: color 0.1s ease-in-out, background-color 0.5s ease-in-out,
+			border 0.5s ease-in-out;
 		user-select: none;
-		position: relative;
+
 		&:hover {
 			color: $textColor;
-			$backgroundSize: 10rem;
-			background-image: repeating-linear-gradient(
-					0deg,
-					$borderColor,
-					$borderColor $backgroundSize,
-					transparent
-				),
-				repeating-linear-gradient(90deg, $borderColor, $borderColor $backgroundSize, transparent),
-				repeating-linear-gradient(180deg, $borderColor, $borderColor $backgroundSize, transparent),
-				repeating-linear-gradient(270deg, $borderColor, $borderColor $backgroundSize, transparent);
+			border-style: solid;
 		}
 		&:active {
-			.tag-click {
-				background-color: #2fc0f465;
-				border-color: #52e2f2c4;
-				transition: unset;
-			}
+			background-color: #2fc0f465;
+			border-color: #52e2f2c4;
+			transition: unset;
 		}
-	}
-
-	.tag-click {
-		transition:
-			background-color 0.5s ease-in-out,
-			border 0.5s ease-in-out;
-		position: absolute;
-		top: 0;
-		left: 0;
-		width: 100%;
-		height: 100%;
-		background-color: transparent;
-		border: 0.75px solid transparent;
 	}
 </style>

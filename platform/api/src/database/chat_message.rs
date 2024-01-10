6972ae0f4c07277a6e8954df3c717c811a4b3d81
Lennart Kloock <lennart.kloock@protonmail.com>
2024-01-10 22:22:18 +0100

    feat(platform): add chat history

--- platform/api/src/database/chat_message.rs
+++ platform/api/src/database/chat_message.rs
@@ -4,12 +4,14 @@ use common::database::Ulid;
 pub struct ChatMessage {
 	/// The unique identifier for the chat message.
 	pub id: Ulid,
-	/// The unique identifier for the chat room which owns the message.
-	pub channel_id: Ulid,
 	/// The unique identifier for the user who sent the message.
 	pub user_id: Ulid,
+	/// The unique identifier for the chat room which owns the message.
+	pub channel_id: Ulid,
 	/// The content of the message.
 	pub content: String,
+	/// The time the message was deleted.
+	pub deleted_at: Option<chrono::DateTime<chrono::Utc>>,
 }
 
 impl ChatMessage {

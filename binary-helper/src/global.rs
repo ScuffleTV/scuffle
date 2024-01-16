961b2fc0112e3e74c1b2e885917bb5b7c83e5053
Troy Benson <troybensonsa@gmail.com>
2024-01-16 04:14:45 +0000

    feat: add tls support

--- binary-helper/src/global.rs
+++ binary-helper/src/global.rs
@@ -90,18 +90,68 @@ pub async fn setup_nats(
 }
 
 pub async fn setup_database(config: &DatabaseConfig) -> anyhow::Result<Arc<common::database::Pool>> {
-	Ok(Arc::new(
-		Pool::builder(common::database::deadpool_postgres::Manager::from_config(
-			config.uri.parse().context("invalid database uri")?,
+	let mut pg_config = config
+		.uri
+		.parse::<common::database::tokio_postgres::Config>()
+		.context("invalid database uri")?;
+
+	pg_config.ssl_mode(if config.tls.is_some() {
+		common::database::tokio_postgres::config::SslMode::Require
+	} else {
+		common::database::tokio_postgres::config::SslMode::Disable
+	});
+
+	let manager = if let Some(tls) = &config.tls {
+		let cert = tokio::fs::read(&tls.cert).await.context("failed to read redis client cert")?;
+		let key = tokio::fs::read(&tls.key)
+			.await
+			.context("failed to read redis client private key")?;
+
+		let key = rustls_pemfile::pkcs8_private_keys(&mut io::BufReader::new(io::Cursor::new(key)))
+			.next()
+			.ok_or_else(|| anyhow::anyhow!("failed to find private key in redis client private key file"))??
+			.into();
+
+		let certs = rustls_pemfile::certs(&mut io::BufReader::new(io::Cursor::new(cert))).collect::<Result<Vec<_>, _>>()?;
+
+		let mut cert_store = RootCertStore::empty();
+		if let Some(ca_cert) = &tls.ca_cert {
+			let ca_cert = tokio::fs::read(ca_cert).await.context("failed to read redis ca cert")?;
+			let ca_certs =
+				rustls_pemfile::certs(&mut io::BufReader::new(io::Cursor::new(ca_cert))).collect::<Result<Vec<_>, _>>()?;
+			for cert in ca_certs {
+				cert_store.add(cert).context("failed to add redis ca cert")?;
+			}
+		}
+
+		let tls = rustls::ClientConfig::builder()
+			.with_root_certificates(cert_store)
+			.with_client_auth_cert(certs, key)
+			.context("failed to create redis tls config")?;
+
+		common::database::deadpool_postgres::Manager::from_config(
+			pg_config,
+			tokio_postgres_rustls::MakeRustlsConnect::new(tls),
+			ManagerConfig {
+				recycling_method: RecyclingMethod::Fast,
+			},
+		)
+	} else {
+		common::database::deadpool_postgres::Manager::from_config(
+			pg_config,
 			NoTls,
 			ManagerConfig {
 				recycling_method: RecyclingMethod::Fast,
 			},
-		))
-		.config(PoolConfig::default())
-		.runtime(Runtime::Tokio1)
-		.build()
-		.context("failed to create database pool")?,
+		)
+	};
+
+	Ok(Arc::new(
+		Pool::builder(manager)
+			.config(PoolConfig::default())
+			.runtime(Runtime::Tokio1)
+			.build()
+			.context("failed to create database pool")?,
 	))
 }
 

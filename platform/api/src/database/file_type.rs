17ee76fc4f3745c334f8bc5c311478785d4386d1
Troy Benson <troybensonsa@gmail.com>
2024-01-17 18:06:52 +0000

    fix: change filetype -> file_type

--- platform/api/src/database/file_type.rs
+++ platform/api/src/database/file_type.rs
@@ -1,7 +1,7 @@
 use postgres_types::{FromSql, ToSql};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, ToSql, FromSql)]
-#[postgres(name = "filetype")]
+#[postgres(name = "file_type")]
 pub enum FileType {
 	#[postgres(name = "custom_thumbnail")]
 	CustomThumbnail,

91ffb028766d77b1c02644239bd67dde321c9d47
Lennart Kloock <lennart.kloock@protonmail.com>
2024-01-24 23:31:35 +0100

    fix(website): image editor

--- platform/website/src/routes/test/+page.svelte
+++ platform/website/src/routes/test/+page.svelte
@@ -1,59 +1,51 @@
 <script lang="ts">
+    // Don't touch this when you don't know what you're doing
+    // You've gotta do the 5Head math shit to understand most of this
+
+    let overlay = true;
+    let debug = false;
+
+    let result: string;
+
     let mouseStartX: number;
     let mouseStartY: number;
     let moveable: HTMLImageElement;
 
     function applyLimits() {
-        const parent = moveable?.parentElement;
-        if (!parent) return;
+        const minLimit = 1 - scale;
+        const maxLimit = 0;
 
-        const width = moveable.offsetWidth;
-        const height = moveable.offsetHeight;
-
-        const xMinLimit = parent.offsetWidth - width * scale;
-        const yMinLimit = parent.offsetHeight - height * scale;
-        const xMaxLimit = 0;
-        const yMaxLimit = 0;
-
-        const magicValueX = (width * (scale - 1)) / 2;
-        const magicValueY = (height * (scale - 1)) / 2;
-
-        const actualX = x - magicValueX;
-        const actualY = y - magicValueY;
+        const s = (scale - 1) / 2;
+        const xl = x - s;
+        const yt = y - s;
 
         // Limit movement to parent element
-        if (actualX > xMaxLimit) {
-            x = xMaxLimit + magicValueX;
-        } else if (actualX < xMinLimit) {
-            x = xMinLimit + magicValueX;
+        if (xl > maxLimit) {
+            x = maxLimit + s;
+        } else if (xl < minLimit) {
+            x = minLimit + s;
         }
-        if (actualY > yMaxLimit) {
-            y = yMaxLimit + magicValueY;
-        } else if (actualY < yMinLimit) {
-            y = yMinLimit + magicValueY;
+        if (yt > maxLimit) {
+            y = maxLimit + s;
+        } else if (yt < minLimit) {
+            y = minLimit + s;
         }
     }
 
     function reset() {
         // Reset scale
         scale = 1;
-        center();
+        // Reset position
+        x = 0;
+        y = 0;
     }
 
-    function center() {
-        const parent = moveable?.parentElement;
-        // Center image
-        if (!parent) return;
-        x = (parent.offsetWidth / 2) - (moveable.offsetWidth * scale / 2);
-        y = (parent.offsetHeight / 2) - (moveable.offsetHeight * scale / 2);
-    }
-
-    // between 0 and 1
+    // (0,0) is left/top, (1,1) is right/bottom
     let x = 0;
     let y = 0;
 
     const minScale = 1;
-    const maxScale = 4;
+    const maxScale = 2;
     // between minScale and maxScale
     let scale = 1;
 
@@ -63,8 +55,10 @@
 
     function mouseDown(e: MouseEvent) {
         e.preventDefault();
-        mouseStartX = e.clientX - x;
-        mouseStartY = e.clientY - y;
+        const xs = x * moveable.offsetWidth;
+        const ys = y * moveable.offsetHeight;
+        mouseStartX = e.clientX - xs;
+        mouseStartY = e.clientY - ys;
         moving = true;
     }
 
@@ -74,8 +68,8 @@
         e.preventDefault();
 
         // Update element position
-        x = e.clientX - mouseStartX;
-        y = e.clientY - mouseStartY;
+        x = (e.clientX - mouseStartX) / moveable.offsetWidth;
+        y = (e.clientY - mouseStartY) / moveable.offsetHeight;
 
         applyLimits();
     }
@@ -99,6 +93,7 @@
         reader.onload = (e) => {
             if (!e.target) return;
             moveable.src = e.target.result as string;
+            calculateResult();
         };
 
         reader.readAsDataURL(file);
@@ -108,74 +103,84 @@
         handleFiles(files[0]);
     }
 
-    function save() {
+    $: calculateResult(), scale, x, y;
+
+    function calculateResult() {
+        if (!moveable) return;
+
         const canvas = document.createElement('canvas');
         const ctx = canvas.getContext('2d');
 
         if (!ctx) return;
 
-        const parent = moveable?.parentElement;
-        if (!parent) return;
+        const xl = x - (scale - 1) / 2;
+        const xr = (x+1) + (scale - 1) / 2;
+        const yt = y - (scale - 1) / 2;
+        const yb = (y+1) + (scale - 1) / 2;
+        console.log("xl,yt,xr,yb", xl, yt, xr, yb);
 
-        const resultDimension = Math.max(moveable.naturalWidth / scale, moveable.naturalHeight / scale);
+        const rw = moveable.naturalWidth / scale;
+        const rh = moveable.naturalHeight / scale;
+        const xrl = (-xl) * rw;
+        const yrt = (-yt) * rh;
 
-        canvas.width = resultDimension;
-        canvas.height = resultDimension;
+        const r = Math.min(rw, rh);
+        const ox = (r - rw) / 2;
+        const oy = (r - rh) / 2;
 
-        let xCorrection = 0;
-        let yCorrection = 0;
-        // if (moveable.naturalWidth > moveable.naturalHeight) {
-        //     yCorrection = (moveable.naturalWidth - moveable.naturalHeight) / 2;
-        // } else if (moveable.naturalHeight > moveable.naturalWidth) {
-        //     xCorrection = (moveable.naturalHeight - moveable.naturalWidth) / 2;
-        // }
+        canvas.width = r;
+        canvas.height = r;
 
-        const magicValueX = (moveable.offsetWidth * (scale - 1)) / 2;
-        const magicValueY = (moveable.offsetHeight * (scale - 1)) / 2;
-        const actualX = ((x - magicValueX) / (moveable.offsetWidth * scale)) * moveable.naturalWidth + xCorrection;
-        const actualY = ((y - magicValueY) / (moveable.offsetHeight * scale)) * moveable.naturalHeight + yCorrection;
-
-        ctx.drawImage(moveable, -actualX, -actualY, resultDimension, resultDimension, 0, 0, resultDimension, resultDimension);
+        console.log(xrl, yrt, rw, rh, ox, oy, rw, rh);
+        ctx.drawImage(moveable, xrl, yrt, rw, rh, ox, oy, rw, rh);
         
-        const data = canvas.toDataURL('image/png');
-
-        const link = document.createElement('a');
-        link.download = 'image.png';
-        link.href = data;
-        link.click();
+        result = canvas.toDataURL('image/png');
     }
 </script>
 
 <svelte:window on:mousemove={mouseMove} on:mouseup={mouseUp} />
 
 <div class="content">
-    <div class="wrapper">
-        <img class="moveable" bind:this={moveable} draggable="false" on:mousedown={mouseDown} on:wheel={wheel} style="--scale: {scale}; --x: {x * moveable.offsetWidth}px; --y: {y * moveable.offsetHeight}px" alt="upload a file" />
-        <div class="mask"></div>
-        <div class="grid">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+    <div class="images">
+        <div class="wrapper" class:debug={debug}>
+            <img class="moveable" bind:this={moveable} draggable="false" on:mousedown={mouseDown} on:wheel={wheel} style="--scale: {scale}; --x: {x * (moveable?.offsetWidth || 0)}px; --y: {y * (moveable?.offsetHeight || 0)}px" alt="upload a file" />
+            {#if overlay}
+                <div class="mask"></div>
+                <div class="grid">
+                    <div></div>
+                    <div class="y-axis"></div>
+                    <div></div>
+                    <div class="x-axis"></div>
+                    <div class="center"></div>
+                    <div class="x-axis"></div>
+                    <div></div>
+                    <div class="y-axis"></div>
+                    <div></div>
+                </div>
+            {/if}
+            {#if debug}
+                <!-- Top left -->
+                <div class="dot" style="--x: {(x - (scale - 1) / 2) * (moveable?.offsetWidth || 0)}px; --y: {(y - (scale - 1) / 2) * (moveable?.offsetHeight || 0)}px;"></div>
+                <!-- Top right -->
+                <div class="dot" style="--x: {((x+1) + (scale - 1) / 2) * (moveable?.offsetWidth || 0)}px; --y: {(y - (scale - 1) / 2) * (moveable?.offsetHeight || 0)}px;"></div>
+                <!-- Bottom left -->
+                <div class="dot" style="--x: {(x - (scale - 1) / 2) * (moveable?.offsetWidth || 0)}px; --y: {((y+1) + (scale - 1) / 2) * (moveable?.offsetHeight || 0)}px;"></div>
+                <!-- Bottom right -->
+                <div class="dot" style="--x: {((x+1) + (scale - 1) / 2) * (moveable?.offsetWidth || 0)}px; --y: {((y+1) + (scale - 1) / 2) * (moveable?.offsetHeight || 0)}px;"></div>
+            {/if}
         </div>
+        <img class="result" src={result} alt="result"/>
     </div>
 
-    <input type="file" bind:files={files}/>
-
-    <button class="button primary" on:click={reset}>Reset</button>
-
+    <div>
+        <input type="file" bind:files={files}/>
+        <button class="button primary" on:click={reset}>Reset</button>
+        <button class="button primary" on:click={() => (overlay = !overlay)}>{overlay ? "Hide" : "Show"} Overlay</button>
+        <button class="button primary" on:click={() => (debug = !debug)}>{debug ? "Hide" : "Show"} Debug</button>
+    </div>
     <input type="range" min={minScale} max={maxScale} step="0.01" bind:value={scale} />
 
     <span>{scale}x</span>
-    <span>x:{x}, y:{y}</span>
-    <span>natural size: {moveable?.naturalWidth}x{moveable?.naturalHeight}</span>
-
-    <button class="button primary" on:click={save}>Save</button>
 </div>
 
 <style lang="scss">
@@ -190,14 +195,30 @@
         height: 100%;
     }
 
-    .wrapper {
+    .images {
+        display: flex;
+        justify-content: center;
+        gap: 2rem;
+        width: 100%;
+        margin-bottom: 2rem;
         margin: 5rem;
+    }
+
+    .result {
+        background-color: $bgColor2;
+        width: 40rem;
+        height: 40rem;
+    }
+
+    .wrapper {
         width: 40rem;
         height: 40rem;
         position: relative;
         background-color: $bgColor;
 
-        overflow: hidden;
+        &:not(.debug) {
+            overflow: hidden;
+        }
     }
 
     .mask {
@@ -234,8 +255,33 @@
         grid-template-rows: repeat(3, 1fr);
 
         & > * {
-            border: 1px solid white;
+            border-style: solid;
+            border-color: white;
+            border-width: 0;
         }
+
+        & > .y-axis {
+            border-left-width: 1px;
+            border-right-width: 1px;
+        }
+
+        & > .x-axis {
+            border-top-width: 1px;
+            border-bottom-width: 1px;
+        }
+
+        & > .center {
+            border-width: 1px;
+        }
+    }
+
+    .dot {
+        position: absolute;
+        transform: translate(var(--x), var(--y)) translate(-50%, -50%);
+        width: 0.5rem;
+        height: 0.5rem;
+        background-color: red;
+        border-radius: 50%;
     }
 
     .moveable {
